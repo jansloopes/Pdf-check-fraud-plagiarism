e3b714202f5beab0ec6675d29fd88e6abda48443
janh <j.f.hellings@hva.nl>
2023-04-20 15:44:13 +0200

    initial commit

---
+++ Code/Pdf-check-fraud-plagiarism.py
@@ -0,0 +1,15 @@
+#
+# print(f"run __init_py in Package_1")
+import Package_2
+
+     
+    
+def main():
+    the_window = Package_2.Create_Windows_Class()
+    the_window.do_window()
+    
+    
+    
+if __name__ == "__main__":
+    main()
+    
